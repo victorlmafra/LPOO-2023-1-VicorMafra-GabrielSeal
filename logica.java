bd5ceaece06e93a6b301c36f4893151d98b25d25
victorlmafra <104863916+victorlmafra@users.noreply.github.com>
2023-06-26 21:28:29 -0300

    Add files via upload

---
+++ logica.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Random;//será implementado para a escolha do jogador que inicia o jogo.
+
+public class Logica {
+    public static void main (string[] args){
+        Quadro[][] FOURIL =new Quadro [7][7];
+        char corAtual = '' //definir qual cor vai ter a vez
+                           //de jogar, ex:azul ou amarelo, ainda não foi implementado
+        string vitoria= " "; //será implementado depois.
+        Boolean game = true;
+        Scanner scan = new Scanner(System.in);
+
+        while(game);
+   
+    }
+
+}
+
+public static void desenhaCampo(Quadro[][] FOURIL){
+    //criar função para limpar a tela;
+    //criar tela através das matrizes;
+    //criar o formato do 4IL;
+    //toda essa parte ainda vai ser implementada.
+
+}
